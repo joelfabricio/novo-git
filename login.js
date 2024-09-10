188ce760bfc1995540d54a34b104a2e0c036751f
jFabricio <joelfabricio@pm.me>
2024-09-10 02:02:39 -0300

    Nova Home final

--- login.js
+++ login.js
@@ -1,3 +1,16 @@
 // Trabalhando no login
 
 // Finalizando sistema de login
+function login(nome, login){
+    return 'bem vindo';
+}
+
+function login(nome, login){
+    return 'bem vindo';
+}
+// Seja bem vindo
+
+// Sistema de Login
+// Retornamos
+
+//Finalizado sistema da home
